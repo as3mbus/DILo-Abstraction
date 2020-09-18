11f4e7d3098e718a5e32386f1be100a35c398a88
A. Billy Firman Prasetya S <billy.firman@agate.id>
2020-09-18 13:43:12 +0700

    Initial State 0 Abstraction

---
+++ Assets/TriggerDetection.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDetection : MonoBehaviour
+{
+    private bool triggerAble = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            if (triggerAble)
+            {
+                Debug.Log($"{name} is triggered");
+            }
+        }
+       
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        triggerAble = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        triggerAble = false;
+    }
+}
---
+++ Assets/movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float speedMod = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 position = transform.position;
+        float x = position.x + Input.GetAxis("Horizontal") * Time.deltaTime * speedMod;
+        float y = position.y + Input.GetAxis("Vertical") * Time.deltaTime * speedMod;
+        transform.position = new Vector3(x, y, position.z);
+    }
+}
